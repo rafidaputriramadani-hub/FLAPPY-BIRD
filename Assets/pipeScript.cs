8f60bce9cdc4f94598e8daad118cdb6b9c356933
rafidaputriramadani-hub <rafidaputriramadani@gmail.com>
2026-01-20 20:05:41 +0800

    project start

---
+++ Assets/pipeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipeScript : MonoBehaviour
+{
+    float nilaiRandom; // nilai desimal
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nilaiRandom = Random.Range(-0.64f, 2.34f);
+        transform.position = new Vector2(transform.position.x, nilaiRandom);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards( transform.position, Vector2.left * 100, Time.deltaTime * 5 );
+    }
+}
---
+++ Assets/playerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // new
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rb; // inisialisasi
+    public float jumpForce; // tinggi loncat
+    float score;
+
+    public Text scoreTxt; // new
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); // pemanggilan inisialisasi
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreTxt.text = "" + score; //new
+        if (Input.GetMouseButtonDown(0)) // klik mouse kiri / tap untuk hp
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Point")
+        {
+            score++;
+        }
+            if(collision.gameObject.tag =="pipa")
+            {
+                Destroy(gameObject);
+        }
+    }
+}
+
